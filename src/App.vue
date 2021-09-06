0b88805fa1a666668b9802ae776d92510debb7f0
ooker <maaker+github@gmail.com>
2021-09-06 22:10:24 +0300

    map styles

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
         <div class="emoji">{{people[index].emoji}}</div>
         <div>
           <h1>{{ people[index].nimi }}</h1>
-          <h2>{{ people[index].hüüdnimi }}</h2>
+          <h2><i>"{{ people[index].hüüdnimi }}"</i></h2>
           <h4>{{ people[index].rühm }}</h4>
         </div>
         <div>
@@ -35,7 +35,6 @@ export default {
     const title = ref("Unchanged Popup Title");
     const sheet = `https://sheets.googleapis.com/v4/spreadsheets/1OXlkK47osO7ZjOyQ3kqLg7_UZmPLG1GGTtLLJHk8Nhw/values/Sheet1?alt=json&majorDimension=ROWS&key=AIzaSyA5ofruKPLx4NuGvdi8OvE3aS6Vi8ADSME`
     let people = ref(null);
-
     let index = ref(-1);
 
     class ClickableMarker extends mapboxgl.Marker {
@@ -54,13 +53,10 @@ export default {
 
     onMounted(() => {
       
-       fetch(sheet)
+      fetch(sheet)
         .then(res => res.json())
         .then(res => {
           people.value = toKeyedRows(res.values);
-          console.log(people.value);
-
-
           people.value.forEach((dude, i) => {
             const el = document.createElement("div");
             el.innerText = dude.emoji;
@@ -73,41 +69,17 @@ export default {
               .addTo(map);
           });
         });
-      
-      
-      
+
       mapboxgl.accessToken = "pk.eyJ1Ijoib29rZXIiLCJhIjoiY2t0OGM4Nm1jMGo4ejJvcXFkZHFzMXdiMyJ9.Mf9gAf6lj40231xxaSjHIQ";
       const map = new mapboxgl.Map({
         container: "map",
-        style: "mapbox://styles/mapbox/light-v9",
-        zoom: 7,
-        center: [24.71454576451196, 58.6350362443377]
+        style: "mapbox://styles/mapbox/streets-v11",
+        bounds: [[21.368220124719926,59.774615762879044], [28.2142900936641,57.437860579312925]]
       });
 
-      map.on("load", () => {
+      // map.on("load", () => {
         
-
-
-        
-
-
-        // // Here we want to setup the dropdown
-        // map.on("click", "usa-fill", function (e) {
-        //   new mapboxgl.Popup()
-        //     .setLngLat(e.lngLat)
-        //     .setHTML('<div id="popup-content"></div>')
-        //     .addTo(map);
-        //   const MyNewPopup = defineComponent({
-        //     extends: MyPopup,
-        //     setup() {
-        //       return { title };
-        //     },
-        //   });
-        //   nextTick(() => {
-        //     createApp(MyNewPopup).mount("#popup-content");
-        //   });
-        // });
-      });
+      // });
     });
 
 
@@ -151,6 +123,7 @@ body{
 }
 .marker{
   font-size: 1.6em;
+  cursor: pointer;
 }
 h1, h2{
   margin: 0;
